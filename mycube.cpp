0989786b5e9f3f6705a51e5cadad916942cbfbbd
riws <belen.varela@udc.es>
2018-02-04 14:20:18 +0100

    Codigo sb7 migrado

--- mycube.cpp
+++ mycube.cpp
@@ -1,6 +1,7 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include "vmath.h"
 
 // keep track of window size for things like the viewport and the mouse cursor
 int g_gl_width = 640;
@@ -19,10 +20,10 @@ int main() {
     return 1;
   }
 
-  //  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  //  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); // We want OpenGL 3.3
   //  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  //  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL 
 
   GLFWwindow* window = glfwCreateWindow(640, 480, "My spinning cube", NULL, NULL);
   if (!window) {
@@ -34,8 +35,11 @@ int main() {
   glfwMakeContextCurrent(window);
 
   // start GLEW extension handler
-  // glewExperimental = GL_TRUE;
-  glewInit();
+  glewExperimental = GL_TRUE; // Needed in core profile of OpenGL
+  if (glewInit() != GLEW_OK) {
+    fprintf(stderr, "Failed to initialize GLEW\n");
+    return 1;
+  }
 
   // get version info
   const GLubyte* renderer = glGetString(GL_RENDERER); // get renderer string
@@ -51,12 +55,28 @@ int main() {
 
   // Vertex Shader
   const char* vertex_shader =
-    // [...]
+    "#version 330 core\n"
+    "in vec4 position;"
+    "out VS_OUT {"
+    "  vec4 color;"
+    "} vs_out;"
+    "uniform mat4 mv_matrix;"
+    "uniform mat4 proj_matrix;"
+    "void main(void) {"
+    "  gl_Position = proj_matrix * mv_matrix * position;"
+    "  vs_out.color = position * 2.0 + vec4(0.5, 0.5, 0.5, 0.0);"
     "}";
+
 
   // Fragment Shader
   const char* fragment_shader =
-    // [...]
+    "#version 330 core\n"
+    "out vec4 color;"
+    "in VS_OUT {"
+    "  vec4 color;"
+    "} fs_in;"
+    "void main(void) {"
+    "  color = fs_in.color;"
     "}";
 
   // Shaders compilation
@@ -73,6 +93,13 @@ int main() {
   glAttachShader(shader_programme, vs);
   glLinkProgram(shader_programme);
 
+  // Uniforms
+  // - Model-View matrix
+  // - Projection matrix
+  GLint mv_location, proj_location;
+  mv_location = glGetUniformLocation(shader_programme, "mv_matrix");
+  proj_location = glGetUniformLocation(shader_programme, "proj_matrix");
+
   // Vertex Array Object
   GLuint vao = 0;
   glGenVertexArrays(1, &vao);
@@ -80,11 +107,60 @@ int main() {
 
   // Cube to be rendered
   const GLfloat vertex_positions[] = {
-    //  [...]
+   -0.25f,  0.25f, -0.25f,
+   -0.25f, -0.25f, -0.25f,
+    0.25f, -0.25f, -0.25f,
+
+    0.25f, -0.25f, -0.25f,
+    0.25f,  0.25f, -0.25f,
+   -0.25f,  0.25f, -0.25f,
+
+    0.25f, -0.25f, -0.25f,
+    0.25f, -0.25f,  0.25f,
+    0.25f,  0.25f, -0.25f,
+
+    0.25f, -0.25f,  0.25f,
+    0.25f,  0.25f,  0.25f,
+    0.25f,  0.25f, -0.25f,
+
+    0.25f, -0.25f,  0.25f,
+   -0.25f, -0.25f,  0.25f,
+    0.25f,  0.25f,  0.25f,
+
+   -0.25f, -0.25f,  0.25f,
+   -0.25f,  0.25f,  0.25f,
+    0.25f,  0.25f,  0.25f,
+
+   -0.25f, -0.25f,  0.25f,
+   -0.25f, -0.25f, -0.25f,
+   -0.25f,  0.25f,  0.25f,
+
+   -0.25f, -0.25f, -0.25f,
+   -0.25f,  0.25f, -0.25f,
+   -0.25f,  0.25f,  0.25f,
+
+   -0.25f, -0.25f,  0.25f,
+    0.25f, -0.25f,  0.25f,
+    0.25f, -0.25f, -0.25f,
+
+    0.25f, -0.25f, -0.25f,
+   -0.25f, -0.25f, -0.25f,
+   -0.25f, -0.25f,  0.25f,
+
+   -0.25f,  0.25f, -0.25f,
+    0.25f,  0.25f, -0.25f,
+    0.25f,  0.25f,  0.25f,
+
+    0.25f,  0.25f,  0.25f,
+   -0.25f,  0.25f,  0.25f,
+   -0.25f,  0.25f, -0.25f
   };
 
   // Vertex Buffer Object(s)
-  // [...]
+  GLuint vbo = 0;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_positions), vertex_positions, GL_STATIC_DRAW);
 
   // Vertex attributes
   // 0: vertex position
@@ -94,12 +170,6 @@ int main() {
   // 1: vertex color
   // [...]
 
-  // Uniforms
-  // - Model-View matrix
-  // - Projection matrix
-  GLint mv_location, proj_location;
-  mv_location = glGetUniformLocation(shader_programme, "mv_matrix");
-  proj_location = glGetUniformLocation(shader_programme, "proj_matrix");
 
   // Render loop
   while(!glfwWindowShouldClose(window)) {
@@ -113,23 +183,24 @@ int main() {
     glUseProgram(shader_programme);
     glBindVertexArray(vao);
 
-    // Suggestion to manage the model-view and projection
-    // transformations:
-    // - use routines and structures from either GLM or
-    //   sb7's vmath libraries
+    vmath::mat4 mv_matrix, proj_matrix;
+    mv_matrix = vmath::translate(0.0f, 0.0f, -4.0f) *
+                                vmath::translate(sinf(2.1f * f) * 0.5f,
+                                                    cosf(1.7f * f) * 0.5f,
+                                                    sinf(1.3f * f) * cosf(1.5f * f) * 2.0f) *
+                                vmath::rotate((float)currentTime * 45.0f, 0.0f, 1.0f, 0.0f) *
+                                vmath::rotate((float)currentTime * 81.0f, 1.0f, 0.0f, 0.0f);
 
-    // ???::mat4 mv_matrix, proj_matrix;
-    // mv_matrix = [...]
-    //
     // transfer mv_matrix uniform to shaders:
-    // glUniformMatrix4fv [...]
+    glUniformMatrix4fv(mv_location, 1, GL_FALSE, mv_matrix);
 
-    // proj_matrix = [...]
-    //
+    float aspect = (float)g_gl_width / (float)g_gl_height;
+    proj_matrix = vmath::perspective(50.0f, aspect, 0.1f, 1000.0f);
+
     // transfer proj_matrix uniform to shaders:
-    // glUniformMatrix4fv [...]
+    glUniformMatrix4fv(proj_location, 1, GL_FALSE, proj_matrix);
 
-    // glDrawArrays [...]
+    glDrawArrays(GL_TRIANGLES, 0, 36);
 
     // update other events like input handling
     glfwPollEvents();
